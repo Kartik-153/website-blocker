da905623f67c727e6ab997fbba3e1d0cd4e2e3c0
kartik <kartikswaroop.007@gmail.com>
2024-12-07 14:03:21 +0530

    modified:   .gitignore 	modified:   chrome_extension/index.html 	modified:   chrome_extension/package-lock.json 	new file:   chrome_extension/public/background.js 	new file:   chrome_extension/public/blocked.html 	modified:   chrome_extension/public/manifest.json 	new file:   chrome_extension/public/rules.json 	new file:   chrome_extension/public/style.css 	new file:   chrome_extension/public/vanish.gif 	modified:   chrome_extension/src/App.tsx

---
+++ chrome_extension/public/background.js
@@ -0,0 +1,30 @@
+const API_URL = "http://127.0.0.1:8000/check-productivity";
+const REDIRECT_URL = chrome.runtime.getURL("blocked.html");
+
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+    try {
+        if (details.frameId !== 0) {
+            return;
+        }
+
+        const { url } = details;
+
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url: url }),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+
+            if (result && (result.productive === 0 || result.productive === false) && result.url) {
+                chrome.tabs.update(details.tabId, { url: REDIRECT_URL });
+            }
+        }
+    } catch (error) {
+        console.error("Error checking productivity:", error);
+    }
+});
