346657e25f81860d3f70f0856b403e70673ad01b
kartik <kartikswaroop.007@gmail.com>
2024-12-21 18:58:27 +0530

    Code Comments

--- api/tensor/src/model.py
+++ api/tensor/src/model.py
@@ -12,20 +12,29 @@ class ProductivityModel:
         self.model.compile()
 
     def predict_productivity(self, image_path: str) -> bool:
+        # Load and preprocess the image
         img = tf.keras.utils.load_img(image_path, target_size=(128, 128))
         img_array = tf.keras.utils.img_to_array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
+        # Predict productivity using the model
         prediction = self.model.predict(img_array)
         return bool(prediction[0][0] < 0.5)
 
     def take_screenshot(self, url: str) -> str:
+        # Initialize the Chrome WebDriver
         driver = webdriver.Chrome()
         driver.get(url)
+        
+        # Wait until the body element is present
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+        
+        # Hash the URL to create a unique filename
         hashed_url = hashlib.sha256(url.encode()).hexdigest()
         image_path = "tensor/dataset/prod/" + hashed_url + ".png"
+        
+        # Save the screenshot and quit the driver
         driver.save_screenshot(image_path)
         driver.quit()
+        
         return image_path
-    
